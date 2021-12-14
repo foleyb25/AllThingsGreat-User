d290bb026e65ce1e0194a28076e00f70e7c597d1
foleyb25 <foleyb25@gmail.com>
2021-12-13 21:29:21 -0700

    Bloggers assorted by super admin status

---
+++ api/controllers/view-bloggers.js
@@ -0,0 +1,12 @@
+module.exports = async function(req,res) {
+    try {
+        const writers = await Writer.find({}).sort('isSuperAdmin DESC')
+        const sanitizedWriters = JSON.parse(JSON.stringify(writers))
+    
+        return res.view("pages/bloggers", {
+          writers: sanitizedWriters,
+        });
+      } catch(err) {
+        res.serverError(error.toString())
+      }
+};
