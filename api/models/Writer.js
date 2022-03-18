1938f5b67905755230fb7cbb7e3d0f341b5eb1e8
foleyb25 <foleyb25@gmail.com>
2022-03-18 12:26:24 -0600

    updating writer data model value is archived

--- api/models/Writer.js
+++ api/models/Writer.js
@@ -16,6 +16,7 @@
       tiktokLink: this.tiktokLink,
       snapchatLink: this.snapchatLink,
       twitterLink: this.twitterLink,
+      isArchived: this.isArchived,
     }
   },
 
@@ -157,6 +158,11 @@ So, while this \`isSuperAdmin\` demarcation might not be the right approach fore
       type: 'number',
       description: 'A JS timestamp (epoch ms) representing the moment at which this user most recently interacted with the backend while logged in (or 0 if they have not interacted with the backend at all yet).',
       example: 1502844074211
+    },
+
+    isArchived: {
+      type: 'boolean',
+      description: 'Whether this user is archived or not. They will not appear in blogger list',
     },
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
