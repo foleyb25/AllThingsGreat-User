e3db2710badebf7bf11c99fef95706dad3dc9756
foleyb25 <foleyb25@gmail.com>
2022-03-18 12:31:55 -0600

    Bloggers filtered out based on archived status

--- api/controllers/view-bloggers.js
+++ api/controllers/view-bloggers.js
@@ -1,6 +1,6 @@
 module.exports = async function(req,res) {
     try {
-        const writers = await Writer.find({}).sort('isSuperAdmin DESC')
+        const writers = await Writer.find({isArchived:false}).sort('isSuperAdmin DESC')
         const sanitizedWriters = JSON.parse(JSON.stringify(writers))
     
         return res.view("pages/bloggers", {
