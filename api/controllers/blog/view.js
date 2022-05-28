07f93b4d0d7041f6602ea84683109b81ba10b179
foleyb25 <foleyb25@gmail.com>
2022-05-28 13:40:59 -0600

    Reverting back last change to use htmlToText package. Not working, need to debug

--- api/controllers/blog/view.js
+++ api/controllers/blog/view.js
@@ -33,12 +33,12 @@ module.exports = async function(req,res) {
       // if (req.wantsJSON) {
       //   return res.send(blog, isRated, isLoggedIn)
       // }
-      // var strippedBlogHtml = sanitizedBlog.bodyHTML.replace(/<[^>]+>/g, '');
-      // const maxLength = 210;
-      // strippedBlogHtml = strippedBlogHtml.substr(0, maxLength);
-      const strippedBlogHtml = htmlToText(sanitizedBlog.bodyHTML)
+      var strippedBlogHtml = sanitizedBlog.bodyHTML.replace(/<[^>]+>/g, '');
       const maxLength = 210;
       strippedBlogHtml = strippedBlogHtml.substr(0, maxLength);
+      // const strippedBlogHtml = htmlToText(sanitizedBlog.bodyHTML)
+      // const maxLength = 210;
+      // strippedBlogHtml = strippedBlogHtml.substr(0, maxLength);
       return res.view("pages/blog/view", {
           blog: sanitizedBlog,
           isRated: isRated,
