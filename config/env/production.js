9e29993623dcbe5c03f8bee5a44df1d9ff92faa9
foleyb25 <foleyb25@gmail.com>
2022-06-26 16:55:39 -0600

    Ditto

--- config/env/production.js
+++ config/env/production.js
@@ -249,7 +249,7 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     onlyAllowOrigins: [
-      'https://allthingsgreat.com',
+      'https://allthingsgreat.herokuapp.com',
     ],
 
 
@@ -368,7 +368,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://allthingsgreat.com',
+    baseUrl: 'https://allthingsgreat.herokuapp.com',
     internalEmailAddress: 'customer_service@allthingsgreat.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
