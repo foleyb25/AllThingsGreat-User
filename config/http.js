107b853a44bf187f9bad6c532ce2739066b0bc07
foleyb25 <foleyb25@gmail.com>
2022-06-26 13:54:18 -0600

    Reverting everything done today

--- config/http.js
+++ config/http.js
@@ -30,16 +30,29 @@ module.exports.http = {
     *                                                                          *
     ***************************************************************************/
 
-    // order: [
-    //   'cookieParser',
-    //   'session',
-    //   'bodyParser',
-    //   'compress',
-    //   'poweredBy',
-    //   'router',
-    //   'www',
-    //   'favicon',
-    // ],
+
+   order: [
+    'cookieParser',
+    'session',
+    'bodyParser',
+    'compress',
+    'poweredBy',
+    'router',
+    'www',
+    'favicon',
+    // 'forceSSL'
+  ],
+
+  // forceSSL: function (req, res, next) {
+
+  //   if (!req.isSecure) {
+  //       return res.redirect('wss://' + req.headers.host + req.url);
+  //   } else if (req.headers["x-forwarded-proto"] == "http") {
+  //       return res.redirect('https://' + req.headers.host + req.url);
+  //   } else {
+  //       next(); //it's already secure
+  //   }
+  // },
 
 
     /***************************************************************************
