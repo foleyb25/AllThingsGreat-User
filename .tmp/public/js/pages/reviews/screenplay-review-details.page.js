a0525a8d0322cf468b0d641e18bcb7b732b1e9b4
foleyb25 <foleyb25@gmail.com>
2022-08-03 11:22:45 -0600

    Changing fear greed details box layout

--- .tmp/public/js/pages/reviews/screenplay-review-details.page.js
+++ .tmp/public/js/pages/reviews/screenplay-review-details.page.js
@@ -1,72 +1,56 @@
-'use strict';
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
 parasails.registerPage('screenplay-review-details', {
-  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
-  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
-  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
-  data: {
-    screenplay: window.SAILS_LOCALS.screenplay,
-    navScreenplays: window.SAILS_LOCALS.navScreenplays
-  },
-
-  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
-  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
-  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function beforeMount() {
-    // Attach any initial data from the server.
-    _.extend(this, SAILS_LOCALS);
-  },
-  mounted: function () {
-    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-      return regeneratorRuntime.wrap(function _callee$(_context) {
-        while (1) {
-          switch (_context.prev = _context.next) {
-            case 0:
-            case 'end':
-              return _context.stop();
-          }
+    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+    data: {
+      screenplay: window.SAILS_LOCALS.screenplay,
+      navScreenplays: window.SAILS_LOCALS.navScreenplays,
+    },
+  
+    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+    beforeMount: function() {
+      // Attach any initial data from the server.
+      _.extend(this, SAILS_LOCALS);
+    },
+    mounted: async function(){
+  
+    },
+  
+    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+    methods: {
+      addkeyframes(review) {
+        const score = review.score*.1*180
+        var bgColor1
+        var bgColor2
+        if (review.score == 10) {
+          bgColor1 = '#b9f2ff'
+          bgColor2 = '#BC64E3'
+        }else if(review.score >= 8 && review.score < 10) {
+          bgColor1 = 'green'
+          bgColor2 = 'green'
+        } else if (review.score < 8 && review.score >= 6) {
+          bgColor1 = 'yellow'
+          bgColor2 = 'yellow'
+        } else {
+          bgColor1 = 'red'
+          bgColor2 = 'red'
         }
-      }, _callee, this);
-    }));
-
-    function mounted() {
-      return _ref.apply(this, arguments);
-    }
-
-    return mounted;
-  }(),
-
-  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
-  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
-  methods: {
-    addkeyframes: function addkeyframes(review) {
-      var score = review.score * .1 * 180;
-      var bgColor1;
-      var bgColor2;
-      if (review.score == 10) {
-        bgColor1 = '#b9f2ff';
-        bgColor2 = '#BC64E3';
-      } else if (review.score >= 8 && review.score < 10) {
-        bgColor1 = 'green';
-        bgColor2 = 'green';
-      } else if (review.score < 8 && review.score >= 6) {
-        bgColor1 = 'yellow';
-        bgColor2 = 'yellow';
-      } else {
-        bgColor1 = 'red';
-        bgColor2 = 'red';
+        return {
+          '--deg': score+'deg',
+          '--bg-gradient': 'linear-gradient(to bottom right, '+bgColor1+', '+bgColor2+')',
+          '--nav-bg-gradient': 'linear-gradient(to bottom right, '+bgColor1+', '+bgColor2+')',
+          '--nav-deg': score+'deg'
+        }
       }
-      return {
-        '--deg': score + 'deg',
-        '--bg-gradient': 'linear-gradient(to bottom right, ' + bgColor1 + ', ' + bgColor2 + ')',
-        '--nav-bg-gradient': 'linear-gradient(to bottom right, ' + bgColor1 + ', ' + bgColor2 + ')',
-        '--nav-deg': score + 'deg'
-      };
-    }
-  },
+    
+    },
 
-  computed: {}
-});
+    computed: {
+      
+    }
+  });
+  
